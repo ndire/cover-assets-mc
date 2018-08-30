a89c547f500e07411ba0932c292c13479ceae255
Nathan Dire <nate.dire@gmail.com>
2018-08-30 14:45:59 -0700

    Play an entire round

--- cya.py
+++ cya.py
@@ -13,7 +13,7 @@ import random
 AssetKind = enum.Enum('AssetKind', [
     'GOLD', 'SILVER', 
     'HOUSE', 'BASEBALL', 'JEWEWLS', 'BANK', 
-    'CARS', 'COINS', 'CASH', 'STOCKS', 'PIGGY'
+    'CARS', 'COINS', 'CASH', 'STOCKS', 'PIGGY', 'STAMPS'
 ])
 
 CARDS = [
@@ -27,6 +27,7 @@ CARDS = [
     (AssetKind.COINS, 10, 10),
     (AssetKind.BASEBALL, 5, 10),
     (AssetKind.CASH, 5, 10),
+    (AssetKind.STAMPS, 5, 10),
     (AssetKind.PIGGY, 5, 10),
 ]
 
@@ -53,16 +54,6 @@ class Card:
     def __repr__(self):
         return str(self)
 
-class Deck:
-
-    def __init__(self, cards):
-        self.cards = cards
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def take(self):
-        return self.cards.pop()
 
 class Player:
 
@@ -74,22 +65,27 @@ class Player:
     def deal(self, card):
         self.hand[card.kind].append(card)
 
-    def name(self):
+    def __str__(self):
         return "Player " + str(self.player_id)
 
     def hand_cards(self):
-        return functools.reduce(operator.add, self.hand.values())
+        return itertools.chain(*self.hand.values())
 
     def replenish(self, game):
-        while len(self.hand_cards()) < HAND_SIZE:
-            self.deal(game.deck.take())
+        while len(list(self.hand_cards())) < HAND_SIZE and game.deck:
+            self.deal(game.deck.pop())
+
+    def total(self):
+        return sum(map(lambda c: c.value, itertools.chain(*self.assets)))
 
     #
     # TODO: make this rule based
     #
     def turn(self, game):
         hand = list(map(str, self.hand_cards()))
-        print("%s hand %s, %s" % (self.name(), hand, game.discard_top()))
+        print("%s hand %s, %s" % (self, hand, game.discard_top()))
+        if not any(self.hand_cards()):
+            return False
 
         match = None
 
@@ -97,33 +93,43 @@ class Player:
         for k, l in self.hand.items():
             if len(l) > 1:
                 match = [l.pop(), l.pop()]
+                print(f"Play {match} from hand")
                 break
 
         # Next check discard.
+        if not match:
+            d = game.discard_top()
+            if d and self.hand[d.kind]:
+                match = [game.discard.pop(), self.hand[d.kind].pop()]
+                print(f"Play {match} using discard")
 
         # Consider stealing.
 
         if match:
             self.assets.append(match)
+        elif any(self.hand_cards()):
+            c = min(self.hand_cards(), key=lambda c: c.value)
+            c = self.hand[c.kind].pop()
+            print(f"Discard {c}")
+            game.discard.append(c)
 
-        print("%s stack %s" % (self.name(), self.assets))
-
+        print("%s stack %s" % (self, self.assets))
         self.replenish(game)
+        return True
 
 class Game:
 
     def __init__(self, n_players=5):
         sets = [list(itertools.repeat(Card(k, amt), n)) for k, amt, n in CARDS]
-        cards = functools.reduce(operator.add, sets)
-        self.deck = Deck(cards)
-        self.deck.shuffle()
+        self.deck = functools.reduce(operator.add, sets)
+        random.shuffle(self.deck)
         self.discard = []
         self.players = [Player(i) for i in range(n_players)]
 
     def deal_hands(self):
         for i in range(HAND_SIZE):
             for p in self.players:
-                p.deal(self.deck.take())
+                p.deal(self.deck.pop())
 
     def discard_top(self):
         if self.discard:
@@ -131,11 +137,17 @@ class Game:
 
     def play(self):
         self.deal_hands()
-        self.discard.append(self.deck.take())
+        self.discard.append(self.deck.pop())
         while True:
-            for p in self.players:
-                p.turn(self)
-            break
+            if not any(p.turn(self) for p in self.players):
+                break
+        s = sum(map(lambda c: c.value, self.discard))
+        print(f"Discard pile {s}")
+        for p in self.players:
+            t = p.total()
+            print(f"{p}: {t}")
+            s += t
+        print(s)
 
 if __name__ == "__main__":
     game = Game()
